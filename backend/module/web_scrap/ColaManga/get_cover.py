13a71a911e2585494604882cdf5c359b51039ba6
unknown <istartgame31@gmail.com>
2024-09-21 00:07:02 +0700

    clean up

--- backend/module/web_scrap/ColaManga/get_cover.py
+++ backend/module/web_scrap/ColaManga/get_cover.py
@@ -14,88 +14,8 @@ import json
 import base64
 import sys
 
-def send(driver, cmd, params={}):
-  resource = "/session/%s/chromium/send_command_and_get_result" % driver.session_id
-  url = driver.command_executor._url + resource
-  body = json.dumps({'cmd': cmd, 'params': params})
-  response = driver.command_executor._request('POST', url, body)
-  return response.get('value')
 
-def scrap(id: int = None, cover_id: int = None):
-    if not id:
-        raise ValueError("The 'id' parameter is required.")
-    if not cover_id:
-        raise ValueError("The 'cover_id' parameter is required.")
-    
-    global scraper
-    
-    try:
-        url = f"https://www.colamanga.com/{id}/"
-
-        if not scraper:
-            scraper = SeleniumScraper()
-        driver = scraper.driver()
-        driver.implicitly_wait(10)
-        driver.get(url)
-        
-        # while True:
-        #     page_state = driver.execute_script('return document.readyState;')
-        #     if page_state == 'complete':
-        #         break
-        
-        image_src_url = f'https://res.colamanga.com/comic/{cover_id}/cover.jpg'
-
-
-            
-        origin_image_element = driver.find_elements(By.CLASS_NAME, "fed-list-pics")[0]
-        
-        # Execute JavaScript to check if the image is fully loaded
-        driver.execute_script(f'arguments[0].innerHTML = "<img id=\\"injected_image\\" src=\\"{image_src_url}\\">";', origin_image_element)
-        
-        image_element = driver.find_element(By.ID, "injected_image")
-        
-        while True:
-            is_image_loaded = driver.execute_script(
-                "return arguments[0].complete", 
-                image_element
-            )
-            if is_image_loaded:
-                break
-
-        DATA = None
-        
-        # Start DevTools session
-        devtools, connection = driver.start_devtools()
-        
-        def process_browser_log_entry(entry):
-            response = json.loads(entry['message'])['message']
-            return response
-
-        browser_log = driver.get_log('performance') 
-        events = [process_browser_log_entry(entry) for entry in browser_log]
-        events = [event for event in events if 'Network.response' in event['method']]
-
-        for e in events:
-            if e.get("params").get("type") == "Image":
-                url = e.get("params").get("response").get("url")
-                if url == image_src_url:
-                    request_id = e["params"]["requestId"]
-                    
-                    response = driver.execute_cdp_cmd('Network.getResponseBody', {'requestId': request_id})
-                    image_data = base64.decodebytes(bytes(response.get("body"), "utf-8"))
-
-                    DATA = image_data
-                    break
-        return DATA
-    
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        line_number = exc_tb.tb_lineno
-        print(f"Error on line {line_number}: {e}")
-        raise Exception(e)
-
-
-def scrap_old(id:int=None,cover_id:int=None):
+def scrap(id:int=None,cover_id:int=None):
     if not id: raise ValueError("The 'id' parameter is required.")
     if not cover_id: raise ValueError("The 'url' parameter is required.")
     global scraper
